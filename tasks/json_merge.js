54fad1da8701a8ad67c6fb5e5c035c9d258746ce
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2014-06-06 11:19:22 +0200

    Initial commit

---
+++ tasks/json_merge.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-json-merge
+ * https://github.com/ramiel/grunt-json-merge
+ *
+ * Copyright (c) 2014 Fabrizio "Ramiel" Ruggeri
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('json_merge', 'Grunt plugin to merge JSON files handling override of keys', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
